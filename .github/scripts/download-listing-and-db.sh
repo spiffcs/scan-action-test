b00604b97478d35ff67f6762c6b09d5a99097096
Keith Zantow <kzantow@gmail.com>
2024-04-03 07:31:07 -0400

    test

---
+++ .github/scripts/download-listing-and-db.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+
+ifconfig
+
+timing_file="$(dirname $0)/curl-timing-info-template.txt"
+
+test_download() {
+  url=$1
+
+  # download with IPv6
+  curl -sL6 -w "@$timing_file" -D /dev/stdout $url -o /dev/null
+
+  # download with IPv4
+  curl -sL4 -w "@$timing_file" -D /dev/stdout $url -o /dev/null
+}
+
+test_download https://toolbox-data.anchore.io/grype/databases/listing.json
+
+# get the latest db file
+db_file="$(curl -sL https://toolbox-data.anchore.io/grype/databases/listing.json | jq -r '.["available"]["5"][0]["url"]')"
+
+test_download $db_file
